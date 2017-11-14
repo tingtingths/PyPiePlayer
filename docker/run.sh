554e0755620ea8e1e445bfd00d68b86ca0d78c9a
Tony Ting <k999hoshing@gmail.com>
2017-11-14 23:36:55 +0800

    Fix shuffle button. Update docker scripts.

--- docker/run.sh
+++ docker/run.sh
@@ -8,7 +8,9 @@ mkdir -p $workdir/sockets \
 	$workdir/log
 
 docker run --name pieplayer -d \
+	--restart always \
+	-m 128m \
     -v $LIBRARY:/music:ro \
 	-v $workdir/sockets:/sockets \
-	-v $workdir:/log:/log \
+	-v $workdir/log:/log \
     tingtingths/pypieplayer:$(cat VERSION)
